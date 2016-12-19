3256c923b35f58ad130d85efdc37c0276e752973
narendra_k <narendra@hashtaag.com>
2016-12-19 17:14:06 +0530

    added login and signup validations

--- app/js/controllers/loginController.js
+++ app/js/controllers/loginController.js
@@ -1,61 +1,95 @@
 app.controller('loginController', function($scope, $location, $rootScope, $window, $http, Auth, $routeParams, $timeout, $cookies, $cookieStore){
   'use strict';
 
-  $scope.Signupform = function() {
-    var signupcredintials = {};
-    // if($scope.signupcredintials) {}
-      signupcredintials = {
-          name: $scope.name,
-          email: $scope.email,
-          password: $scope.password,
-        }
+  $('.modal').on('hidden.bs.modal', function (e) {
+    $(this).find("input").val('').end();
+    $scope.errorMessage =  "";
+  })
 
-
-    Auth.register(signupcredintials).success(function(data) {
-    // localStorage.setItem("token", data.token);
-    // localStorage.setItem("userId", data._id);
-    // localStorage.setItem("name", data.email);
-    //localStorage.setItem("loggedIn", true);
-    //cookieStore
-     $cookieStore.put("token", data.token);
-     $cookieStore.put("userId", data._id);
-     $cookieStore.put("email", data.email);
-     $cookieStore.put('loggedIn', true);
-    $location.path('/cart-description');
-    //location.reload(true);
-    alert ("Account Created successfully")
-    $scope.dirty = false;
-  }).error(function(data) {
-    console.log('data', data);
-      alert ("Account Created unsuccess")
-  });
-
+  $scope.regexEmail = new RegExp(/^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/);
+  $scope.Signupform = function(signupform) {
+    var regexEmail = new RegExp(/^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/);
+    $scope.errorMessage = "";
+      var signupcredintials = {};
+    if (!$scope.signupform.$valid) {
+      $scope.erroralert = true;
+      if (signupform.$error.required) {
+        $scope.errorMessage = "All fields required";
+      } else if (!regexEmail.test($scope.email)) {
+          $scope.errorMessage = "Please enter a valid email id";
+      } else if (signupform.$error.minlength) {
+          $scope.errorMessage = 'Minimum password length 6 required';
+      }
+    } else {
+        // if($scope.signupcredintials) {}
+          signupcredintials = {
+              name: $scope.name,
+              email: $scope.email,
+              password: $scope.password,
+            }
+        Auth.register(signupcredintials).success(function(data) {
+        // localStorage.setItem("token", data.token);
+        // localStorage.setItem("userId", data._id);
+        // localStorage.setItem("name", data.email);
+        //localStorage.setItem("loggedIn", true);
+        //cookieStore
+         $cookieStore.put("token", data.token);
+         $cookieStore.put("userId", data._id);
+         $cookieStore.put("email", data.email);
+         $cookieStore.put('loggedIn', true);
+         $location.path('/landing');
+         location.reload(true);
+        //location.reload(true);
+        alert ("Account Created successfully")
+      }).error(function(data) {
+        console.log('data', data);
+        $scope.erroralert = true;
+        $scope.errorMessage = data.errors.email.message;
+      });
+    };
+    $timeout(function () {
+      $scope.errorMessage = "";
+    }, 3000);
   }
 
   $scope.loginform = function () {
-    Auth.login ({
-      email: $scope.email,
-      password: $scope.password,
-    }).success(function (data) {
-      // localStorage.setItem("token", data.token);
-      // localStorage.setItem("userId", data._id);
-      // localStorage.setItem("email", data.email);
-      // localStorage.setItem('loggedIn', true);
+    $scope.erroralert = false;
+    $scope.errorMessage = "";
+    if (!$scope.krazyloginform.$valid) {
+      $scope.errorMessage = "All fields required";
+      $scope.erroralert = true;
+    } else {
+      Auth.login ({
+        email: $scope.email,
+        password: $scope.password,
+      }).success(function (data) {
+        // localStorage.setItem("token", data.token);
+        // localStorage.setItem("userId", data._id);
+        // localStorage.setItem("email", data.email);
+        // localStorage.setItem('loggedIn', true);
 
-      //cookieStore
-       $cookieStore.put("token", data.token);
-       $cookieStore.put("userId", data._id);
-       $cookieStore.put("email", data.email);
-       $cookieStore.put('loggedIn', true);
+        //cookieStore
+         $cookieStore.put("token", data.token);
+         $cookieStore.put("userId", data._id);
+         $cookieStore.put("email", data.email);
+         $cookieStore.put('loggedIn', true);
 
-       $scope.userId = $cookieStore.get('userId');
-       console.log("user id ",$scope.userId);
+         $scope.userId = $cookieStore.get('userId');
+         console.log("user id ",$scope.userId);
+         $('.modal').css("display", "none");
+         $('.modal-open').removeClass();
+          $location.path('/landing');
+          location.reload(true);
 
-      $location.path('/cart-description');
-    }).error(function(data) {
-      console.log('data', data);
-        alert ("login failed")
-    });
+      }).error(function(data) {
+        console.log('data', data);
+          $scope.erroralert = true;
+          $scope.errorMessage = data.message;
+      });
+    }
+    $timeout(function () {
+      $scope.errorMessage = "";
+    }, 3000);
   }
 
   $scope.logout = function () {
@@ -69,7 +103,8 @@ app.controller('loginController', function($scope, $location, $rootScope, $windo
     $cookieStore.remove("email");
     $cookieStore.put('loggedIn', false);
 
-    $location.path('/login');
+    location.reload();
+    $location.path('/landing');
   }
   $scope.product = function() {
     Auth.products().success(function(data) {
