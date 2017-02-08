c225f99c344784f5db5b0a22762188680ce63381
darshankumar89 <darshankumar38@gmail.com>
2017-02-08 14:37:43 +0530

    DK [first section landing page slider and UI tweaks ]

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -38,7 +38,7 @@ app.controller('mainController', function($scope, $location, $rootScope, $window
 
 
   //section slider
-  // $scope.myInterval = 8000;
+  $scope.myInterval = 8000;
   $scope.noWrapSlides = false;
 
 //   var slideIndex = 0;
@@ -60,6 +60,35 @@ app.controller('mainController', function($scope, $location, $rootScope, $window
 //     setTimeout($scope.showSlides, 6000); // Change image every 2 seconds
 // }
 
+  //create session id
+
+    $scope.createUUID = function() {
+      var s = [];
+      var hexDigits = "0123456789abcdef";
+      for (var i = 0; i < 36; i++) {
+          s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+      }
+      s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+      s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+      s[8] = s[13] = s[18] = s[23] = "-";
+
+      var uuid = s.join("");
+      console.log("uuid",uuid);
+      //cookie store
+      //cookieStore
+       $cookieStore.put("sessionId", uuid);
+      return uuid;
+    }
+
+    if ($cookieStore.get('sessionId')) {
+      //$scope.createUUID();
+      console.log("cookie will not store sessionId as there is  session id");
+    } else {
+      console.log("cookie will  store sessionId ");
+      $scope.createUUID();
+    }
+
+    console.log("cookie",$cookieStore.get('sessionId'));
 
   //slider
 
