7af72a703534f26f37e8f9d04ee61d948ad041bb
Rajat Yadav <rajat@hashtaag.co>
2017-02-27 14:42:17 +0530

    home sliding menu fixed

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -94,14 +94,14 @@ app.controller('mainController', function($scope, $location, $rootScope, $window
 
 $('#prv-testimonial').on('click', function(){
     var $last = $('#testimonial-list .slide-list:last');
-    $last.remove().css({ 'margin-left': '-200px' });
+    $last.remove().css({ 'margin-left': '-248px' });
     $('#testimonial-list .slide-list:first').before($last);
     $last.animate({ 'margin-left': '0px' }, 1000);
 });
 
 $('#nxt-testimonial').on('click', function(){
     var $first = $('#testimonial-list .slide-list:first');
-    $first.animate({ 'margin-left': '-200px' }, 1000, function() {
+    $first.animate({ 'margin-left': '-248px' }, 1000, function() {
         $first.remove().css({ 'margin-left': '0px' });
         $('#testimonial-list .slide-list:last').after($first);
     });
