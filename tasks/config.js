545647423c966e09f3814f7f0ac62b5ea9451317
narendra_k <narendra@hashtaag.com>
2017-03-28 12:31:32 +0530

    added small tweak

--- tasks/config.js
+++ tasks/config.js
@@ -30,7 +30,7 @@ var config =  function() {
     // Server Configuration
     var serverConfiguration = {
         host       : 'localhost',
-        port       : 3001,
+        port       : 3000,
         open       : true,
         livereload : {
             enable: true,
